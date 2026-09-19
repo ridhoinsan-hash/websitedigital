e6464d16b698eb8b20cb1ee256230262e030eb8a
Ryder_18Th <ridhoinsanztc@gmail.com>
2026-09-19 10:45:46 +0700

    Update MySQL password in config.php

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@ mysqli_report(MYSQLI_REPORT_OFF);
 
 $host     = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $user     = getenv('MYSQLUSER') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: 'CW5KbnkHvDRdcuCJPHGuBFsFsaucnm11';
+$password = getenv('MYSQLPASSWORD') ?: 'IPUkvcZRPBKoqTqbKyrnkcedxdWUEpmR';
 $database = getenv('MYSQLDATABASE') ?: 'railway';
 $port     = (int)(getenv('MYSQLPORT') ?: 3306);
 
